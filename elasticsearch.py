0c5a7aa796c1f38c06349d04d380528256a6f007
sacreman <sacreman@gmail.com>
2014-11-20 12:20:20 +0000

    made elasticsearch plugin awesome

--- elasticsearch.py
+++ elasticsearch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import requests
 import collections
 
@@ -18,24 +19,11 @@ HEALTH_URL = "/_cluster/health"
 NODES_URL = "/_nodes/stats?all=true"
 
 
-def _get_es_version(url):
-    """ Get the running version of Elasticsearch
-    """
-    data = requests.get(url)
-    if data.status_code == 200:
-        # print data.json()
-        version = data.json()['version']['number']
-        return version
-    else:
-        raise Exception("Cannot get Elasticsearch version")
-
-
 def _get_es_stats(url):
     """ Get the node stats
     """
     data = requests.get(url)
     if data.status_code == 200:
-        # print data.json()
         stats = data.json()
         return stats
     else:
@@ -46,30 +34,30 @@ def flatten(d, parent_key='', sep='.'):
     """ flatten a dictionary into a dotted string
     """
     items = []
-    for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
-            items.extend(flatten(v, new_key, sep=sep).items())
+    for key, value in d.items():
+        new_key = parent_key + sep + key if parent_key else key
+        if isinstance(value, collections.MutableMapping):
+            items.extend(flatten(value, new_key, sep=sep).items())
         else:
-            items.append((new_key, v))
+            items.append((new_key, value))
     return dict(items)
 
 
 try:
-    es_ver = _get_es_version(BASE_URL)
-    # print es_ver
+    es_stats = flatten(_get_es_stats(BASE_URL + NODES_URL))
+    es_health = flatten(_get_es_stats(BASE_URL + HEALTH_URL))
 
-    es_stats = _get_es_stats(BASE_URL + NODES_URL)
-    # print es_stats
-
-    es_stats_flat = flatten(es_stats)
+    all_stats = dict(es_stats.items() + es_health.items())
 
     perf_data = "OK | "
-    for k,v in es_stats_flat.iteritems():
-        k = k.rsplit('.')[2::]
-        perf_data +=  'elasticsearch.' + '.'.join(k) + '=' + str(v) + ';;;; '
+    for k, v in all_stats.iteritems():
+        if str(v)[0].isdigit():
+            k = k.rsplit('.')[2::]
+            perf_data += '.'.join(k) + '=' + str(v) + ';;;; '
 
     print(perf_data)
+    sys.exit(0)
 
 except Exception as e:
     print("Exception: " + str(e))
+    sys.exit(2)
