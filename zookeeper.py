bf7c73f4ecbb9c8d3496b2a04e1bac4e6fe016d6
sacreman <sacreman@gmail.com>
2014-12-23 13:37:59 +0000

    adds zookeeper check script

---
+++ zookeeper.py
@@ -0,0 +1,148 @@
+#! /usr/bin/env python
+import sys
+import socket
+import re
+import subprocess
+from StringIO import StringIO
+
+HOST = 'localhost'
+PORT = 2181
+
+class ZooKeeperServer(object):
+
+    def __init__(self, host=HOST, port=PORT, timeout=1):
+        self._address = (host, int(port))
+        self._timeout = timeout
+
+    def get_stats(self):
+        """ Get ZooKeeper server stats as a map """
+        data = self._send_cmd('mntr')
+        if data:
+            return self._parse(data)
+        else:
+            data = self._send_cmd('stat')
+            return self._parse_stat(data)
+
+    def _create_socket(self):
+        return socket.socket()
+
+    def _send_cmd(self, cmd):
+        """ Send a 4letter word command to the server """
+        s = self._create_socket()
+        s.settimeout(self._timeout)
+
+        s.connect(self._address)
+        s.send(cmd)
+
+        data = s.recv(2048)
+        s.close()
+
+        return data
+
+    def _parse(self, data):
+        """ Parse the output from the 'mntr' 4letter word command """
+        h = StringIO(data)
+        
+        result = {}
+        for line in h.readlines():
+            try:
+                key, value = self._parse_line(line)
+                result[key] = value
+            except ValueError:
+                pass # ignore broken lines
+
+        return result
+
+    def _parse_stat(self, data):
+        """ Parse the output from the 'stat' 4letter word command """
+        h = StringIO(data)
+
+        result = {}
+        
+        version = h.readline()
+        if version:
+            result['zk_version'] = version[version.index(':')+1:].strip()
+
+        # skip all lines until we find the empty one
+        while h.readline().strip(): pass
+
+        for line in h.readlines():
+            m = re.match('Latency min/avg/max: (\d+)/(\d+)/(\d+)', line)
+            if m is not None:
+                result['zk_min_latency'] = int(m.group(1))
+                result['zk_avg_latency'] = int(m.group(2))
+                result['zk_max_latency'] = int(m.group(3))
+                continue
+
+            m = re.match('Received: (\d+)', line)
+            if m is not None:
+                result['zk_packets_received'] = int(m.group(1))
+                continue
+
+            m = re.match('Sent: (\d+)', line)
+            if m is not None:
+                result['zk_packets_sent'] = int(m.group(1))
+                continue
+
+            m = re.match('Outstanding: (\d+)', line)
+            if m is not None:
+                result['zk_outstanding_requests'] = int(m.group(1))
+                continue
+
+            m = re.match('Mode: (.*)', line)
+            if m is not None:
+                result['zk_server_state'] = m.group(1)
+                continue
+
+            m = re.match('Node count: (\d+)', line)
+            if m is not None:
+                result['zk_znode_count'] = int(m.group(1))
+                continue
+
+        return result 
+
+    def _parse_line(self, line):
+        try:
+            key, value = map(str.strip, line.split('\t'))
+        except ValueError:
+            raise ValueError('Found invalid line: %s' % line)
+
+        if not key:
+            raise ValueError('The key is mandatory and should not be empty')
+
+        try:
+            value = int(value)
+        except (TypeError, ValueError):
+            pass
+
+        return key, value
+
+
+def get_cluster_stats():
+    """ Get stats for all the servers in the cluster """
+    stats = {}
+    try:
+        zk = ZooKeeperServer(HOST, PORT)
+        stats["%s:%s" % (HOST, PORT)] = zk.get_stats()
+
+    except socket.error, e:
+        # ignore because the cluster can still work even 
+        # if some servers fail completely
+
+        # this error should be also visible in a variable
+        # exposed by the server in the statistics
+        print "unable to connect to server %s on port %s" % (HOST, PORT)
+        sys.exit(2)
+
+    return stats
+
+cluster_stats = get_cluster_stats()
+
+output = "OK | "
+for host, info in cluster_stats.iteritems():
+    for k, v in info.iteritems():
+        if 'zk_version' not in k:
+            output += "%s=%s;;;; " % (k, v)
+
+print output
+
