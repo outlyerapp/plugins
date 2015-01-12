921139503a564695425482f1b161aa15e5a6a22d
sacreman <sacreman@gmail.com>
2015-01-12 13:03:52 +0000

    improves the mysql plugin

--- mysql.py
+++ mysql.py
@@ -3,9 +3,19 @@ import subprocess
 import re
 import sys
 
-status = subprocess.check_output(['/usr/local/bin/mysqladmin', 'status'])
-status = re.sub(': ', '=', status)
-status = re.sub('  ', ';;;; ', status)
+"""
+Open the mysql console on the server and run:
+    grant usage on *.* to 'dataloop'@'localhost' identified by 'dataloop';
+
+Or alternatively change the -udataloop and -pdataloop to a mysql user that can run the status command
+"""
+try:
+    status = subprocess.check_output(['/usr/bin/mysqladmin', '-udataloop', '-pdataloop', 'status'])
+    status = re.sub(': ', '=', status)
+    status = re.sub('  ', ';;;; ', status)
+except:
+    sys.exit(2)
 
 print 'OK | %s' % status
 sys.exit(0)
+
