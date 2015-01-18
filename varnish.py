51442631e263a845d53af931e8a0397ea83d61f5
sacreman <sacreman@gmail.com>
2015-01-18 17:22:17 +0000

    adds varnish plugin

---
+++ varnish.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+
+command = "/usr/bin/varnishstat -1 | awk '{print $1,$2}'"
+
+try:
+    status = subprocess.check_output(command, shell=True)
+except:
+    print "connection failure"
+    sys.exit(2)
+
+output = "OK | "
+metrics_list = [s.lower().strip() for s in status.splitlines()]
+for metric in metrics_list:
+    k = metric.split(' ')[0]
+    v = metric.split(' ')[1]
+    output += k + '=' + v + ';;;; '
+
+print output
+sys.exit(0)
+
