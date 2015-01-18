7fb438cd4b89caa593775f14292f54645b92cb5e
sacreman <sacreman@gmail.com>
2015-01-18 15:44:49 +0000

    updates mysql plugin

--- mysql.py
+++ mysql.py
@@ -4,18 +4,32 @@ import re
 import sys
 
 """
-Open the mysql console on the server and run:
-    grant usage on *.* to 'dataloop'@'localhost' identified by 'dataloop';
-
-Or alternatively change the -udataloop and -pdataloop to a mysql user that can run the status command
+This script runs /usr/bin/mysqladmin status and cuts up the output into Nagios format.
+You may need to update the MYSQL_USER and MYSQL_PASSWORD with an account that can connect. 
 """
+
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = ''
+
+command = ['/usr/bin/mysqladmin', 'status']
+if MYSQL_USER:
+    command.append('-u%s' % MYSQL_USER)
+if MYSQL_PASSWORD:
+    command.append('-p%s' % MYSQL_PASSWORD)
+
 try:
-    status = subprocess.check_output(['/usr/bin/mysqladmin', '-udataloop', '-pdataloop', 'status'])
-    status = re.sub(': ', '=', status)
-    status = re.sub('  ', ';;;; ', status)
+    status = subprocess.check_output(command)
 except:
+    print "connection failure"
     sys.exit(2)
 
-print 'OK | %s' % status
+output = "OK | "
+metric_list = status.split('  ')
+for metric in metric_list:
+    k = metric.split(':')[0].lower().replace(' ', '_').strip()
+    v = metric.split(':')[1].strip()
+    output += k + '=' + v + ';;;; '
+
+print output
 sys.exit(0)
 
