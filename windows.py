49d200f8495ebe726259501dea799ad4c6e701fa
sacreman <sacreman@gmail.com>
2015-01-30 16:52:47 +0000

    fix up windows script

--- windows.py
+++ windows.py
@@ -5,19 +5,18 @@ import re
 
 counters = ['\Memory\Available MBytes',
             '\Memory\Pages/sec',
-            '\PhysicalDisk(* *)\% Disk Time',
-            '\PhysicalDisk(* *)\Current Disk Queue Length',
-            '\PhysicalDisk(* *)\Disk Transfers/sec',
-            '\PhysicalDisk(* *)\Disk Bytes/sec',
-            '\PhysicalDisk(* *)\Disk Reads/sec',
-            '\PhysicalDisk(* *)\Disk Writes/sec',
-            '\PhysicalDisk(* *)\Avg. Disk sec/Read',
-            '\PhysicalDisk(** *)\Avg. Disk sec/Write',
-            '\Processor(* *)\% Processor Time',
-            '\Processor(* *)\% Privileged Time',
+            '\PhysicalDisk(_Total)\% Disk Time',
+            '\PhysicalDisk(_Total)\Current Disk Queue Length',
+            '\PhysicalDisk(_Total)\Disk Transfers/sec',
+            '\PhysicalDisk(_Total)\Disk Bytes/sec',
+            '\PhysicalDisk(_Total)\Disk Reads/sec',
+            '\PhysicalDisk(_Total)\Disk Writes/sec',
+            '\PhysicalDisk(_Total)\Avg. Disk sec/Read',
+            '\PhysicalDisk(_Total)\Avg. Disk sec/Write',
+            '\Processor(_Total)\% Processor Time',
+            '\Processor(_Total)\% Privileged Time',
             '\System\Processor Queue Length',
-            '\System\Context Switches/sec',
-            '\Network Interface(* *)\Bytes Total/sec']
+            '\System\Context Switches/sec']
 
 
 command = [r'c:\windows\system32\typeperf.exe', '-sc', '1']
@@ -33,6 +32,7 @@ i = 1
 for counter in counters:
     metric = counter.split('\\')[2].lower()
     metric = re.sub('[^0-9a-zA-Z]+', '_', metric)
+    metric = re.sub('(^[\W_]*)|([\W_]*$)', '', metric)
     value = output.splitlines()[2].split(',')[i].replace('"','').strip()
     perf_data[metric] = value
     i += 1
