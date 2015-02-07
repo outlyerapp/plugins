66834df8ceacab4fd1fe84d21cbb013eb0dcd530
sacreman <sacreman@gmail.com>
2015-02-07 02:39:23 +0000

    adds hadoop datanode and jobtracker

---
+++ hadoop-datanode.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import requests
+import json
+import socket
+
+names = ['*']
+
+excludes = ['namenodeaddresses',
+            'volumeinfo',
+            'httpport',
+            'name',
+            'version',
+            'modelertype',
+            'storageinfo',
+            'clusterid',
+            'rpcport',
+            'tag.port',
+            'tag.context',
+            'tag.hostname',
+            'tag.sessionid',
+            'tag.processname']
+    
+metrics = {}
+for n in names:
+    URL = 'http://%s:50075/jmx?qry=Hadoop:service=DataNode,name=%s' % (socket.getfqdn(), n)
+    try:
+        resp = requests.get(URL).json()['beans']
+        for i in range(len(resp)):
+            for k, v in resp[i].iteritems():
+                if k.lower() not in excludes:
+                    metrics[k] = v
+    
+    except Exception, e:
+        print "connection failed: %s" % e
+        sys.exit(2)
+
+output = "OK | "
+for k, v in metrics.iteritems():
+    output += str(k).lower() + '=' + str(v).lower() + ';;;; '
+
+print output
+
