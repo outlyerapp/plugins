cf13d2e18a26fcc36e60d144503c9c95f5759019
sacreman <sacreman@gmail.com>
2015-02-07 21:57:41 +0000

    adds yarn node manager

---
+++ yarn-nodemgr.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import sys
+import requests
+import json
+import socket
+
+names = ['*']
+
+excludes = ['tag.context',
+            'modelertype',
+            'tag.port',
+            'tag.processname',
+            'tag.sessionid',
+            'name',
+            'tag.hostname']
+    
+metrics = {}
+node_data = {}
+
+for n in names:
+    URL = 'http://%s:8042/jmx?qry=Hadoop:service=NodeManager,name=%s' % (socket.getfqdn(), n)
+    try:
+        resp = requests.get(URL).json()['beans']
+        for i in range(len(resp)):
+            for k, v in resp[i].iteritems():
+                if k.lower() not in excludes:
+                    metrics[k] = v
+
+    
+    except Exception, e:
+        print "connection failed: %s" % e
+        sys.exit(2)
+
+output = "OK | "
+for k, v in metrics.iteritems():
+    output += str(k).lower() + '=' + str(v).lower() + ';;;; '
+
+print output
+
