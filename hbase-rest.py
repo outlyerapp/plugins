c2f69388e6efce00a3c393c794d0afda665c7206
sacreman <sacreman@gmail.com>
2015-02-07 22:13:02 +0000

    adds hbase rest

---
+++ hbase-rest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+import requests
+import json
+import socket
+
+names = ['*']
+
+excludes = ['name',
+            'modelertype']
+    
+metrics = {}
+node_data = {}
+
+for n in names:
+    URL = 'http://%s:8085/jmx?qry=hadoop:service=rest,name=%s' % (socket.getfqdn(), n)
+    try:
+        resp = requests.get(URL).json()['beans']
+        for i in range(len(resp)):
+            for k, v in resp[i].iteritems():
+                if k.lower() not in excludes:
+                    metrics[k] = v
+
+    
+    except Exception, e:
+        print "connection failed: %s" % e
+        sys.exit(2)
+
+output = "OK | "
+for k, v in metrics.iteritems():
+    output += str(k).lower() + '=' + str(v).lower() + ';;;; '
+
+print output
+
