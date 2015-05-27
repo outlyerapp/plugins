a1e047cfe1d47103f7d11a2d3f9fdc33cf5b3e71
sacreman <sacreman@gmail.com>
2015-05-27 16:18:18 +0100

    updated cadvisor with docker and host metrics

--- cadvisor.py
+++ cadvisor.py
@@ -2,6 +2,8 @@
 
 import requests
 import os
+import sys
+import pprint
 from docker import Client
 
 try:
@@ -12,10 +14,85 @@ except:
     print "connection failed"
     sys.exit(2)
 
+metrics = {}
+
+# Docker Stats
+
+info = c.info()
+metrics['ngoroutines'] = info['NGoroutines']
+metrics['driverstatus.dirs'] = info['DriverStatus'][2][1]
+metrics['containers.running'] = len(c.containers())
+metrics['containers.total'] = info['Containers']
+metrics['images.total'] = info['Images']
+metrics['images.committed'] = len(c.images(quiet=True))
+    
+# Host Stats
+host_stats = requests.get("%s/api/v2.0/stats?count=1" % cadvisor_uri).json()
+
+# Network
+for index, elem in enumerate(host_stats['/'][0]['network']):
+    metrics['host.network.%s.tx_dropped' % index] = host_stats['/'][0]['network'][index]['tx_dropped']
+    metrics['host.network.%s.rx_packets' % index] = host_stats['/'][0]['network'][index]['rx_packets']
+    metrics['host.network.%s.rx_bytes' % index] = host_stats['/'][0]['network'][index]['rx_bytes']
+    metrics['host.network.%s.tx_errors' % index] = host_stats['/'][0]['network'][index]['tx_errors']
+    metrics['host.network.%s.rx_errors' % index] = host_stats['/'][0]['network'][index]['rx_errors']
+    metrics['host.network.%s.tx_bytes' % index] = host_stats['/'][0]['network'][index]['tx_bytes']
+    metrics['host.network.%s.rx_dropped' % index] = host_stats['/'][0]['network'][index]['rx_dropped']
+    metrics['host.network.%s.tx_packets' % index] = host_stats['/'][0]['network'][index]['tx_packets']
+    
+# Load Stats
+metrics['host.load_stats.nr_stopped'] = host_stats['/'][0]['load_stats']['nr_stopped']
+metrics['host.load_stats.nr_sleeping'] = host_stats['/'][0]['load_stats']['nr_sleeping']
+metrics['host.load_stats.nr_uninterruptible'] = host_stats['/'][0]['load_stats']['nr_uninterruptible']
+metrics['host.load_stats.nr_running'] = host_stats['/'][0]['load_stats']['nr_running']
+metrics['host.load_stats.nr_io_wait'] = host_stats['/'][0]['load_stats']['nr_io_wait']
+
+# Disk IO
+for index, elem in enumerate(host_stats['/'][0]['diskio']['io_service_bytes']):
+    metrics['host.diskio.io_service_bytes.%s.major' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['major']
+    metrics['host.diskio.io_service_bytes.%s.minor' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['minor']
+    metrics['host.diskio.io_service_bytes.%s.stats.read' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['stats']['Read']
+    metrics['host.diskio.io_service_bytes.%s.stats.async' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['stats']['Async']
+    metrics['host.diskio.io_service_bytes.%s.stats.write' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['stats']['Write']
+    metrics['host.diskio.io_service_bytes.%s.stats.total' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['stats']['Total']
+    metrics['host.diskio.io_service_bytes.%s.stats.sync' % index] = host_stats['/'][0]['diskio']['io_service_bytes'][index]['stats']['Sync']
+
+for index, elem in enumerate(host_stats['/'][0]['diskio']['io_serviced']):    
+    metrics['host.diskio.io_serviced.%s.major' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['major']
+    metrics['host.diskio.io_serviced.%s.minor' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['minor']
+    metrics['host.diskio.io_serviced.%s.stats.read' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['stats']['Read']
+    metrics['host.diskio.io_serviced.%s.stats.async' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['stats']['Async']
+    metrics['host.diskio.io_serviced.%s.stats.write' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['stats']['Write']
+    metrics['host.diskio.io_serviced.%s.stats.total' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['stats']['Total']
+    metrics['host.diskio.io_serviced.%s.stats.sync' % index] = host_stats['/'][0]['diskio']['io_serviced'][index]['stats']['Sync']
+    
+# Memory
+metrics['host.memory.usage'] = host_stats['/'][0]['memory']['usage']
+metrics['host.memory.working_set'] = host_stats['/'][0]['memory']['working_set']
+metrics['host.memory.hierarchical_data.pgfault'] = host_stats['/'][0]['memory']['hierarchical_data']['pgfault']
+metrics['host.memory.hierarchical_data.pgmajfault'] = host_stats['/'][0]['memory']['hierarchical_data']['pgmajfault']
+metrics['host.memory.container_data.pgfault'] = host_stats['/'][0]['memory']['container_data']['pgfault']
+metrics['host.memory.container_data.pgmajfault'] = host_stats['/'][0]['memory']['container_data']['pgmajfault']
+
+# CPU
+metrics['host.cpu.usage.system'] = host_stats['/'][0]['cpu']['usage']['system']
+metrics['host.cpu.usage.total'] = host_stats['/'][0]['cpu']['usage']['total']
+metrics['host.cpu.usage.user'] = host_stats['/'][0]['cpu']['usage']['user']
+metrics['host.cpu.load_average'] = host_stats['/'][0]['cpu']['load_average']
+
+for index, elem in enumerate(host_stats['/'][0]['cpu']['usage']['per_cpu_usage']):
+    metrics['host.cpu.usage.per_cpu_usage.%s' % index] = host_stats['/'][0]['cpu']['usage']['per_cpu_usage'][index]
+        
+
+# Container Stats
+
+raw_stats = {}
+
 def get_stats(container_id):
     return requests.get("%s/api/v2.0/stats/%s?type=docker&count=1" % (cadvisor_uri, container_id)).json()
 
-raw_stats = {}
+
+
 for container in containers:
     raw_stats.update({container['Id']:{ 'name': container['Names'],
                                         'image': container['Image'],
@@ -23,7 +100,7 @@ for container in containers:
                                       }
                     })
 
-metrics = {}
+
 
 def store_metric(name, image, path, value):
     metrics[name + '.' + path] = value
