c917bafc7ec4d362f3dcd04f192d3448a6a02208
sacreman <sacreman@gmail.com>
2015-05-27 15:15:25 +0100

    update rabbit

--- rabbitmq.py
+++ rabbitmq.py
@@ -8,7 +8,6 @@ USERNAME = "guest"
 PASSWORD = "guest"
 PORT = "15672"
 
-#url = "http://%s:%s/api/queues/%%2f/%s" % (HOST, PORT, QUEUE)
 
 URL = "http://%s:%s/api/overview" % (HOST, PORT)
 
@@ -21,23 +20,35 @@ def get_data(url):
 overview = get_data("http://%s:%s/api/overview" % (HOST, PORT))
 
 metrics = {}
-metrics['total_messages'] = overview['queue_totals']['messages']
-metrics['total_messages_rate'] = overview['queue_totals']['messages_details']['rate']
-metrics['total_messages_ready'] = overview['queue_totals']['messages_ready']
-metrics['total_messages_ready_rate'] = overview['queue_totals']['messages_ready_details']['rate']
-metrics['total_messages_unack'] = overview['queue_totals']['messages_unacknowledged']
-metrics['total_messages_unack_rate'] = overview['queue_totals']['messages_unacknowledged_details']['rate']
-metrics['total_consumers'] = overview['object_totals']['consumers']
-metrics['total_queues'] = overview['object_totals']['queues']
-metrics['total_exhanges'] = overview['object_totals']['exchanges']
-metrics['total_connections'] = overview['object_totals']['connections']
-metrics['total_channels'] = overview['object_totals']['channels']
+
+# Message Stats
+metrics['message_stats.publish'] = overview['message_stats']['publish']
+metrics['message_stats.publish_rate'] = overview['message_stats']['publish_details']['rate']
+metrics['message_stats.deliver_get'] = overview['message_stats']['deliver_get']
+metrics['message_stats.deliver_get_rate'] = overview['message_stats']['deliver_get_details']['rate']
+metrics['message_stats.confirm'] = overview['message_stats']['confirm']
+metrics['message_stats.confirm_rate'] = overview['message_stats']['confirm_details']['rate']
+metrics['message_stats.deliver_no_ack'] = overview['message_stats']['deliver_no_ack']
+metrics['message_stats.deliver_no_ack_rate'] = overview['message_stats']['deliver_no_ack_details']['rate']
+
+# Queue Totals
+metrics['queue_totals.messages'] = overview['queue_totals']['messages']
+metrics['queue_totals.rate'] = overview['queue_totals']['messages_details']['rate']
+metrics['queue_totals.messages_ready'] = overview['queue_totals']['messages_ready']
+metrics['queue_totals.messages_ready_rate'] = overview['queue_totals']['messages_ready_details']['rate']
+metrics['queue_totals.unack'] = overview['queue_totals']['messages_unacknowledged']
+metrics['queue_totals.unack_rate'] = overview['queue_totals']['messages_unacknowledged_details']['rate']
+
+# Object Totals
+metrics['object_totals.consumers'] = overview['object_totals']['consumers']
+metrics['object_totals.queues'] = overview['object_totals']['queues']
+metrics['object_totals.exhanges'] = overview['object_totals']['exchanges']
+metrics['object_totals.connections'] = overview['object_totals']['connections']
+metrics['object_totals.channels'] = overview['object_totals']['channels']
 
 perf_data = "OK | "
 for k, v in metrics.iteritems():
     perf_data += "%s=%s;;;; " % (k, v)
 
 print perf_data
-
-
 
