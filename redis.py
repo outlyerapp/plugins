5f142c1e56b98a320477881e079a46fddc22403a
sacreman <sacreman@gmail.com>
2015-09-01 00:10:44 +0100

    split redis metrics for db0

--- redis.py
+++ redis.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import sys
 import subprocess
+import re
 
 try:
-    output = subprocess.check_output(('redis-cli', 'info'))
-except:
-    print "Plugin Failed!"
+    output = subprocess.check_output(('/usr/local/bin/redis-cli', 'info'))
+except Exception, e:
+    print "Plugin Failed! %s" % e
     sys.exit(2)
 
 metrics = output.split()
@@ -29,11 +30,15 @@ exclusion_list = ['redis_version',
 
 perf_data = "OK | "
 for m in metrics:
-    if ":" in m:
+    if 'db0' in m:
+        keys = m[9:].split(',')[0]
+        perf_data += "keys=%s;;;; " % keys
+    elif ":" in m:
         k = m.split(':')[0]
         v = m.split(':')[1]
         if k not in exclusion_list:
             perf_data += "%s=%s;;;; " % (k, v)
+
 
 
 print perf_data
