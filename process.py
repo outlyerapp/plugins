00cf55b640c969eeb8b1c7288fb89c1d78adf5f2
sacreman <sacreman@gmail.com>
2015-11-13 12:37:36 +0000

    make the process plugin better

--- process.py
+++ process.py
@@ -2,12 +2,57 @@
 import psutil
 import sys
 
-PROCESS_NAME = "syslogd"
+"""
+update required_processes with a dictionary of process name and how many should be running
+"""
 
-for process in psutil.process_iter():
-    if process.name() == PROCESS_NAME:
-        print "OK - %s is up and running" % PROCESS_NAME
-        sys.exit(0)
+required_processes = {"bash": 5,
+                      "launchd": 4}
 
-print "CRITICAL! Process %s is not running" % PROCESS_NAME
-sys.exit(2)
+
+# wrap getting the process name with exception handling
+def get_proc_name(proc):
+    try:
+        return proc.name()
+    except psutil.AccessDenied:
+        # IGNORE: we don't have permission to access this process
+        pass
+    except psutil.NoSuchProcess:
+        # IGNORE: process has died between listing and getting info
+        pass
+    except Exception, e:
+        print "error accessing process info: %s" % e
+    return None
+
+
+# get all of the process counts
+running_processes = {}
+for p in psutil.process_iter():
+    if get_proc_name(p) in running_processes:
+        running_processes[get_proc_name(p)] += 1
+    else:
+        running_processes[get_proc_name(p)] = 1
+
+# print the counts and exit correctly
+output = ""
+exit_status = 0
+for process, number in required_processes.iteritems():
+    if process in running_processes and required_processes[process] == running_processes[process]:
+        # the process name and count are correct
+        output += str(process) + '.count=' + str(running_processes[process]) + ';;;; '
+    else:
+        # the process is running but the count is wrong
+        if process in running_processes:
+            output += str(process) + '.count=' + str(running_processes[process]) + ';;;; '
+            exit_status = 2
+        # the process isn't running at all
+        else:
+            output += str(process) + '.count=0;;;; '
+            exit_status = 2
+
+if exit_status == 0:
+    print "OK | " + output
+    sys.exit(0)
+else:
+    print "FAIL | " + output
+    sys.exit(2)
