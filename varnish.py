d5a6a58adf4971803ae6122385d0a9b93186a193
sacreman <sacreman@gmail.com>
2015-11-18 16:51:50 +0000

    update varnish with rate calculation

--- varnish.py
+++ varnish.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python
 import subprocess
 import sys
+import time
 
 command = "/usr/bin/varnishstat -1 | awk '{print $1,$2}'"
 
-try:
-    status = subprocess.check_output(command, shell=True)
-except:
-    print "Plugin Failed!"
-    sys.exit(2)
 
+def get_varnish_metrics():
+    try:
+        status = subprocess.check_output(command, shell=True)
+        m = {}
+        for line in status.splitlines():
+            name = line.split()[0].lower().replace('(', '_').replace(')', '_').replace(',,', '_')
+            m[name] = line.split()[1]
+        return m
+
+    except Exception, e:
+        print "Plugin Failed! %s" % e
+        sys.exit(2)
+
+
+# rate calculation
+time_between = 5
+
+metrics_before = get_varnish_metrics()
+time.sleep(time_between)
+metrics = get_varnish_metrics()
+
+metric_rates = {}
+for metric, value in metrics.iteritems():
+    if value > metrics_before[metric]:
+        metric_rates[metric + '_per_sec'] = (int(value) - int(metrics_before[metric])) / time_between
+
+metrics.update(metric_rates)
+
+# print nagios perf data and exit
 output = "OK | "
-metrics_list = [s.lower().strip() for s in status.splitlines()]
-for metric in metrics_list:
-    k = metric.split(' ')[0]
-    v = metric.split(' ')[1]
-    output += k + '=' + v + ';;;; '
+for k, v in metrics.iteritems():
+    output += str(k) + '=' + str(v) + ';;;; '
 
 print output
 sys.exit(0)
+
+
+
+
 
