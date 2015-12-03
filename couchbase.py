0e4089b7b6181c55bacba1759c5b978806b7930b
sacreman <sacreman@gmail.com>
2015-12-03 10:11:36 -0800

    made the couchbase plugin more awesome

--- couchbase.py
+++ couchbase.py
@@ -1,63 +1,63 @@
 #!/usr/bin/env python
-"""
-Update the USER and PASSWORD variables below.
-"""
-
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+"""
+
+Update the USER and PASSWORD variables below.
+
+"""
+
 USER = ''
 PASSWORD = ''
 
-output = "OK | "
-exit_status = 0
+URL = 'http://localhost:8091/pools/default'
 auth = HTTPBasicAuth(USER, PASSWORD)
-perf_data = {}
-
 try:
-    default = requests.get('http://localhost:8091/pools/default', auth=auth, timeout=60).json()
+    resp = requests.get(URL, auth=auth, timeout=60).json()
 except:
-    print "Plugin Failed! Unable to connect to http://localhost:8091/pools/default"
+    print "Plugin Failed! Unable to connect to %s" % URL
     sys.exit(2)
 
-# The top level metrics
-perf_data['maxbucketcount'] = default['maxBucketCount']
-perf_data['rebalance_success'] = default['counters']['rebalance_success']
-perf_data['rebalance_start'] = default['counters']['rebalance_start']
-perf_data['hdd_storage.free'] = default['storageTotals']['hdd']['free']
-perf_data['hdd_storage.total'] = default['storageTotals']['hdd']['total']
-perf_data['hdd_storage.quota_total'] = default['storageTotals']['hdd']['quotaTotal']
-perf_data['hdd_storage.used_by_data'] = default['storageTotals']['hdd']['usedByData']
-perf_data['hdd_storage.used'] = default['storageTotals']['hdd']['used']
-perf_data['ram.used'] = default['storageTotals']['ram']['used']
-perf_data['ram.quota_used'] = default['storageTotals']['ram']['quotaUsed']
-perf_data['ram.quota_used_per_node'] = default['storageTotals']['ram']['quotaUsedPerNode']
-perf_data['ram.quota_total_per_node'] = default['storageTotals']['ram']['quotaTotalPerNode']
-perf_data['ram.total'] = default['storageTotals']['ram']['total']
-perf_data['ram.quota_total'] = default['storageTotals']['ram']['quotaTotal']
-perf_data['ram.used_by_data'] = default['storageTotals']['ram']['usedByData']
 
-# Node level metrics
-nodes = default['nodes']
-for node in nodes:
-    node_name = node['hostname'].split('.')[0]
-    if node['clusterMembership'] != 'active' and node['thisNode']:
-        output = "Node: %s is not active | " % node
-        exit_status = 2
-    perf_data[node_name + '.' + 'mcd_memory_reserved'] = node['mcdMemoryReserved']
-    perf_data[node_name + '.' + 'mcd_memory_allocated'] = node['mcdMemoryAllocated']
-    perf_data[node_name + '.' + 'memory_free'] = node['memoryFree']
-    perf_data[node_name + '.' + 'memory_total'] = node['memoryTotal']
-    system = node['systemStats']
-    for k, v in system.iteritems():
-        perf_data[node_name + '.' + k] = v
-    interesting = node['interestingStats']
-    for k, v in interesting.iteritems():
-        perf_data[node_name + '.' + k] = v
+def flatten(structure, key="", path="", flattened=None):
+    if flattened is None:
+        flattened = {}
+    if type(structure) not in (dict, list):
+        flattened[((path + ".") if path else "") + key] = structure
+    elif isinstance(structure, list):
+        for i, item in enumerate(structure):
+            flatten(item, "%d" % i, path + "." + key, flattened)
+    else:
+        for new_key, value in structure.items():
+            flatten(value, new_key, path + "." + key, flattened)
+    return flattened
 
-for k, v in perf_data.iteritems():
-    output += str(k) + '=' + str(v) + ';;;; '
 
-print output
-sys.exit(exit_status)
+def bytes_to_gb(num):
+    return round(float(num) / 1024 / 1024 / 1024, 2)
+
+
+success_message = "OK | "
+failure_message = "FAILED! | "
+perf_data = ""
+healthy = True
+
+for node in resp['nodes']:
+    if node['status'] != 'healthy':
+        healthy = False
+
+for k, v in flatten(resp).iteritems():
+    if type(v) is int or type(v) is float:
+        metric_path =  k.lower().replace('..', '')
+        perf_data += metric_path + '=' + str(v) + ';;;; '
+        if 'storagetotals' in metric_path:
+            perf_data += metric_path + '_gb=' + str(bytes_to_gb(v)) + 'GB;;;; '
+
+if healthy:
+    print success_message + perf_data
+    sys.exit(0)
+else:
+    print failure_message + perf_data
+    sys.exit(2)
