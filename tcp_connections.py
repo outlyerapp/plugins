6e789e88a01789ae73809df0bcdcd92663d11132
sacreman <sacreman@gmail.com>
2015-12-18 14:50:22 +0000

    tcp_connections only report established

--- tcp_connections.py
+++ tcp_connections.py
@@ -8,9 +8,9 @@ cmap = {'in': 0,
         'out': 0 }
 
 for c in psutil.net_connections(kind='inet'):
-    if len(c.laddr) > 0 and c.laddr[1] == PORT:
+    if len(c.laddr) > 0 and c.laddr[1] == PORT and c.status == 'ESTABLISHED':
         cmap['in'] += 1
-    if len(c.raddr) > 0 and c.raddr[1] == PORT:
+    if len(c.raddr) > 0 and c.raddr[1] == PORT and c.status == 'ESTABLISHED':
         cmap['out'] += 1
 
 print "OK | connections_in=%d;;;; connections_out=%d;;;;" % (cmap['in'], cmap['out'])
