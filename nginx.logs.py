9e8f6967a6fd32597fe8a2edabab3da283f0cded
sacreman <sacreman@gmail.com>
2016-02-09 11:34:56 +0000

    read nginx log file in reverse rather than store state in a tmpfile

--- nginx.logs.py
+++ nginx.logs.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import os
+import re
+from datetime import datetime
+import time
+import sys
+
 """
 Reads through an nginx access log file and extracts a count of http status codes.
 
@@ -16,79 +22,52 @@ access_log /var/log/nginx/yourdomain.com.access.log timed_combined;
 
 """
 
-import re
-import os
-import sys
-
-TMPDIR = '/opt/dataloop/tmp'
-
-## Change these two variables if you are duplicating this script. Otherwise you will overwrite the filepointer held in the tmpfile.
-TMPFILE = 'dl-nginx-access'
 LOGFILE = '/var/log/nginx/access.log'
 
 combined = '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"'
 timed_combined = '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$request_time"'
+timezone = time.strftime("%z",time.localtime()) 
+start_time = datetime.now()
 
-status_codes={'2xx': 0,
-              '3xx': 0,
-              '4xx': 0,
-              '5xx': 0}
-times = {}
-times['count'] = 0
-times['total'] = 0
-times['max'] = 0
-offset = 0
+status_codes = {'2xx': 0,
+                '3xx': 0,
+                '4xx': 0,
+                '5xx': 0}
+times = {
+    'count': 0,
+    'total': 0,
+    'max': 0
+}
 
+def reverse_read(fname, separator=os.linesep):
+    with file(fname) as f:
+        f.seek(0, 2)
+        fsize = f.tell()
+        r_cursor = 1
+        while r_cursor <= fsize:
+            a_line = ''
+            while r_cursor <= fsize:
+                f.seek(-1 * r_cursor, 2)
+                r_cursor += 1
+                c = f.read(1)
+                if c == separator and a_line:
+                    r_cursor -= 1
+                    break
+                a_line += c
+            a_line = a_line[::-1]
+            yield a_line
 
-def tmp_file():
-    # Ensure the dataloop tmp dir is available
-    if not os.path.isdir(TMPDIR):
-        os.makedirs(TMPDIR)
-    if not os.path.isfile(TMPDIR + '/' + TMPFILE):
-        os.mknod(TMPDIR + '/' + TMPFILE)
-
-
-def get_offset():
-    with open(TMPDIR + '/' + TMPFILE, 'r') as off:
-        try:
-            offset = int(off.read())
-        except:
-            # unable to read offset file
-            offset = 0
-    return offset
-
-
-def write_offset(offset):
-    with open(TMPDIR + '/' + TMPFILE, 'w') as off:
-        off.write("%s" % offset)
-
-# Flow
-tmp_file()
-
-# Get the last know log file read position
-# to mitigate logrotation, if the file is smaller than offset, read the whole
-# log file again
-position = get_offset()
-file_size = os.stat(LOGFILE).st_size
-if file_size < position:
-    position = 0
-
-with open(LOGFILE) as fp:
-    if position > 0:
-        fp.seek(position)
-    for line in fp.xreadlines():
-        regex = ''.join('(?P<' + g + '>.*?)' if g else re.escape(c) for g, c in re.findall(r'\$(\w+)|(.)', timed_combined))
+for line in reverse_read(LOGFILE):
+    regex = ''.join('(?P<' + g + '>.*?)' if g else re.escape(c) for g, c in re.findall(r'\$(\w+)|(.)', timed_combined))
+    m = re.match(regex, line)
+    if not m:
+        regex = ''.join('(?P<' + g + '>.*?)' if g else re.escape(c) for g, c in re.findall(r'\$(\w+)|(.)', combined))
         m = re.match(regex, line)
-        if not m:
-            regex = ''.join('(?P<' + g + '>.*?)' if g else re.escape(c) for g, c in re.findall(r'\$(\w+)|(.)', combined))
-            m = re.match(regex, line)
+    data = m.groupdict()
 
-        try:
-            data = m.groupdict()
-        except:
-            print "Plugin Failed!"
-            sys.exit(2)
-
+    line_time = datetime.strptime(data['time_local'], '%d/%b/%Y:%H:%M:%S '+ timezone)
+    delta = start_time - line_time
+    if delta.seconds < 30:
         code = data['status']
         if code not in status_codes.keys():
             status_codes[code] = 1
@@ -101,7 +80,7 @@ with open(LOGFILE) as fp:
         if code.startswith('4'):
             status_codes['4xx'] += 1
         if code.startswith('5'):
-            status_codes['5xx'] += 1                
+            status_codes['5xx'] += 1
         if 'request_time' in data.iterkeys():
             time_taken = data['request_time']
             if 'min' not in times.iterkeys():
@@ -113,13 +92,11 @@ with open(LOGFILE) as fp:
             if time_taken < times['min']:
                 times['min'] = time_taken
 
-    write_offset(fp.tell())
-    fp.close()
 
-# emit lots of lovely metrics:
+            
 message = "OK | "
-for k,v in status_codes.iteritems():
-    message += "%s=%s;;;; " % (k,v)
+for k, v in status_codes.iteritems():
+    message += "%s=%s;;;; " % (k, v)
 
 if times['count'] > 0:
     message += "avg_time=%0.2fs;;;; " % (float(times['total'])/float(times['count']))
