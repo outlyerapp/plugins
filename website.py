aba4b8a6224a4bf80bc70daab91315c2e58c0843
sacreman <sacreman@gmail.com>
2016-08-18 22:30:10 +0100

    remove content-length from website check script

--- website.py
+++ website.py
@@ -4,16 +4,15 @@ import requests
 from requests import exceptions
 import sys
 
-URL = 'https://www.google.com'
-STRING = '<title>Google</title>'
+URL = 'https://www.gradschool.psu.edu'
+STRING = '<a href="/prospective-students/how-to-apply/">How to Apply</a>'
 
 try:
     response = requests.get(URL, verify=True)
 
     if STRING in response.content:
-        print "OK | status_code=%s;;;; time=%s;;;; size=%s;;;;" % (str(response.status_code),
-                                                                   str(response.elapsed.total_seconds()) + 's',
-                                                                   str(response.headers['content-length']) + 'B')
+        print "OK | status_code=%s;;;; time=%s;;;;" % (str(response.status_code),
+                                                       str(response.elapsed.total_seconds()) + 's')
         sys.exit(0)
     else:
         print "Plugin Failed! String %s not found" % STRING
