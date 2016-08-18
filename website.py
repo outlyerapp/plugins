509d6ca031c67a9e736563dc1bc4d24a6d49f7a6
sacreman <sacreman@gmail.com>
2016-08-18 22:09:42 +0100

    add ssl verify to website script

--- website.py
+++ website.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
 import requests
+from requests import exceptions
 import sys
 
 URL = 'https://www.google.com'
 STRING = '<title>Google</title>'
 
-response = requests.get(URL)
+try:
+    response = requests.get(URL, verify=True)
 
-if STRING in response.content:
-    print "OK | status_code=%s;;;; time=%s;;;; size=%s;;;;" % (str(response.status_code), 
-                                                               str(response.elapsed.total_seconds()) + 's',
-                                                               str(response.headers['content-length']) + 'B')
-    sys.exit(0)
-else:
-    print "Plugin Failed!"
+    if STRING in response.content:
+        print "OK | status_code=%s;;;; time=%s;;;; size=%s;;;;" % (str(response.status_code),
+                                                                   str(response.elapsed.total_seconds()) + 's',
+                                                                   str(response.headers['content-length']) + 'B')
+        sys.exit(0)
+    else:
+        print "Plugin Failed! String %s not found" % STRING
+        sys.exit(2)
+
+except exceptions.SSLError:
+    print "Plugin Failed! SSL Certificate verification failed."
     sys.exit(2)
+
+
 
