407acc1c3c0a882f4f75dacc6d84bc6d2b69dc0f
Oliver Greenaway <oliver.greenaway@outlyer.com>
2017-04-27 18:26:11 +0100

    working for windows and linux

--- ping.py
+++ ping.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 import sys
-from threading import Thread
 import subprocess
-from Queue import Queue
+import re
+import platform
 
 '''
-This script pings a list of addresses and returns the status and round trip stats for each
+This script pings an address and returns the status and round trip stats for each
 '''
 
-addresses = ["www.google.com"]
-num_threads = 4
+addresses = "microsoft.com"
 num_pings = 5
 
 
@@ -17,38 +16,52 @@ queue = Queue()
 metrics = {}
 
 
-def ping(i, q):
-    while True:
-        address = q.get()
-        cmd = "ping -c %i %s" % (num_pings, address)
-        try:
-            cmd_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
-        except subprocess.CalledProcessError:
-            metrics[address + '.status'] = 2
-        else:
-            address = address.replace('.', '_')
-            metrics[address + '.status'] = 0
-            cmd_output = filter(None, cmd_output.split('\n'))[-1].split('=')[1].replace(' ms', '')
-            rtt = cmd_output.split('/')
-            rtt = [x.strip(' ') for x in rtt]
-            metrics[address + '.min'] = rtt[0] + 'ms'
-            metrics[address + '.avg'] = rtt[1] + 'ms'
-            metrics[address + '.max'] = rtt[2] + 'ms'
-            metrics[address + '.stddev'] = rtt[3] + 'ms'
-        q.task_done()
+def ping_linux(i, q):
+    address = q
+    cmd = "ping -c %i %s" % (num_pings, address)
+    try:
+        cmd_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+    except subprocess.CalledProcessError:
+        metrics[address + '.status'] = 2
+    else:
+        address = address.replace('.', '_')
+        metrics[address + '.status'] = 0
+        cmd_output = filter(None, cmd_output.split('\n'))[-1].split('=')[1].replace(' ms', '')
+        rtt = cmd_output.split('/')
+        rtt = [x.strip(' ') for x in rtt]
+        metrics[address + '.min'] = rtt[0] + 'ms'
+        metrics[address + '.avg'] = rtt[1] + 'ms'
+        metrics[address + '.max'] = rtt[2] + 'ms'
+        metrics[address + '.stddev'] = rtt[3] + 'ms'
+    return metrics
 
 
-for i in range(num_threads):
-    worker = Thread(target=ping, args=(i, queue))
-    worker.setDaemon(True)
-    worker.start()
+def ping_windows(i, q):
+    regex = r'\n[\s]{4}Minimum\s=\s(?P<min>[0-9]+(ms|s)),\sMaximum\s=\s(?P<max>[0-9]+(ms|s)),\sAverage\s=\s(?P<avg>[0-9]+(ms|s))'
+    address = q
+    cmd = "ping -n %i %s" % (num_pings, address)
+    try:
+        cmd_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+    except subprocess.CalledProcessError:
+        metrics[address + '.status'] = 2
+    else:
+        address = address.replace('.', '_')
+        metrics[address + '.status'] = 0
+        #print cmd_output
+        processed_results = re.search(regex, cmd_output, re.M)
+        #print processed_results
+        metrics[address + '.min'] = str(processed_results.group('min'))
+        metrics[address + '.avg'] = str(processed_results.group('avg'))
+        metrics[address + '.max'] = str(processed_results.group('max'))
+    return metrics
+
+if platform.system() == "Windows":
+    metrics = ping_windows(num_pings, addresses)
+if platform.system() == "Linux":
+    metrics = ping_linux(num_pings, addresses)
 
 
-for a in addresses:
-    queue.put(a)
-
-queue.join()
-
+#print metrics
 output = "OK | "
 exit_code = 0
 for k, v in metrics.iteritems():
